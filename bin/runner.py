db74ebe42500722a24a42bb801f38a0e1619bfb8
Emir Cem Gezer <emircem.gezer@gmail.com>
2020-03-29 20:22:53 +0300

    fix: disable cors

--- bin/runner.py
+++ bin/runner.py
@@ -1,14 +1,11 @@
 import json
 from flask import Flask
 from flask_restful import Api
-from flask_cors import CORS
 
 from apitizer import img_parser
 
 app = Flask(__name__)
 api = Api(app)
-
-CORS(app)
 
 with open('config/config.json') as f:
     config = json.load(f)
