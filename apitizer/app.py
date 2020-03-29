59e89ba0b730e1b8758e7a1690b34b1c2138ed84
Emir Cem Gezer <emircem.gezer@gmail.com>
2020-03-29 22:30:29 +0000

    chore: adjustment for flask

---
+++ apitizer/app.py
@@ -0,0 +1,23 @@
+import json
+from flask import Flask
+from flask_restful import Api
+
+from apitizer import img_parser
+
+app = Flask(__name__)
+api = Api(app)
+
+with open('config/config.json') as f:
+    config = json.load(f)
+
+parser = img_parser.ImageParser(config)
+
+
+@app.route("/", methods=['GET'])
+def daily():
+    return parser.get_results()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
